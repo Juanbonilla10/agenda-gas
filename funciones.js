fbfa7317607c3a1bbc1deee52dcad8049d47e318
Juan Coronado <coronadobonillajuancarlos904@gmail.com>
2025-01-13 20:53:52 -0500

    video 9 gas avanzado

--- funciones.js
+++ funciones.js
@@ -6,3 +6,11 @@ function obtenerDatosHtml(nombre)
 {
      return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos(){
+
+    let hoja = SpreadsheetApp.openById('1jjlsFJfwn3hxqad0XYbQ1B_vUIuYJldHyeRxUUhUJAk').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+
+}
