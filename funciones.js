422e3b15238390a8966b30db85914f260732291d
Juan Coronado <coronadobonillajuancarlos904@gmail.com>
2025-01-14 21:16:14 -0500

    video 15 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,4 +1,10 @@
+const HOJA = SpreadsheetApp.openById('1jjlsFJfwn3hxqad0XYbQ1B_vUIuYJldHyeRxUUhUJAk').getActiveSheet();
+
 function doGet() {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
+}
+function doPost(datos)
+{
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
 }
 
@@ -8,9 +14,10 @@ function obtenerDatosHtml(nombre)
 }
 
 function obtenerContactos(){
+    return HOJA.getDataRange().getValues();
+}
 
-    let hoja = SpreadsheetApp.openById('1jjlsFJfwn3hxqad0XYbQ1B_vUIuYJldHyeRxUUhUJAk').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
-
+function insertarContacto(nombre,correo)
+{
+    HOJA.appendRow([nombre,correo]);
 }
